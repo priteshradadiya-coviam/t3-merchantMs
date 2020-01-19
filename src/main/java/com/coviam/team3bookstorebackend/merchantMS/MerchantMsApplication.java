f7c529f22012ccfe9bb5fca125f8c6ace6524d60
priteshradadiya-coviam <59829370+priteshradadiya-coviam@users.noreply.github.com>
2020-01-19 16:24:01 +0530

    initial commit

---
+++ src/main/java/com/coviam/team3bookstorebackend/merchantMS/MerchantMsApplication.java
@@ -0,0 +1,13 @@
+package com.coviam.team3bookstorebackend.merchantMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchantMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coviam/team3bookstorebackend/merchantMS/MerchantMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coviam.team3bookstorebackend.merchantMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
